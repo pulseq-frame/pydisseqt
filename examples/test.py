41ac986cab63ae2c3ce70aa4d53a600449b78d9e
J-Endres <67545816+J-Endres@users.noreply.github.com>
2023-12-27 21:09:46 +0100

    First WIP providig .pyi typing stubs

---
+++ examples/test.py
@@ -0,0 +1,90 @@
+from pydissect import SeqParser
+
+with open("examples/test.seq") as f:
+    parser = SeqParser(f.read())
+
+
+# Example: Instantaneous pulse
+t0 = parser.next(0, "pulse_start")
+t1 = parser.next(0, "pulse_end")
+
+_, g1 = parser.integrate(t0, (t0 + t1) / 2)
+pulse, _ = parser.integrate(t0, t1)
+_, g2 = parser.integrate((t0 + t1) / 2, t1)
+
+
+# Example: ADC readout
+t = parser.next(0, "adc_start")
+t_end = parser.next(0, "adc_end")
+
+while t < t_end:
+    t_next = parser.next(t, "adc_sample")
+    _, grad = parser.integrate(t, t_next)
+    t = t_next
+    _, _, (adc_phase, adc_freq) = parser.sample(t)
+    # Simulate gradient moment and then do an ADC sample
+
+_, grad = parser.integrate(t, t_end)
+# Simulate gradient moment after last sample
+
+
+# Example application: Plot seq file
+t_start, t_end = parser.time_range()
+
+# Plot pulses
+t = t_start
+while t < t_end:
+    t = parser.next(t, "pulse_start")
+    if not t:
+        break
+    t_pulse_end = parser.next(t, "pulse_end")
+
+    (_, _, pulse_freq), _, _ = parser.sample((t_start + t_end) / 2)
+
+    time_shape = []
+    amp_shape = []
+    phase_shape = []
+    while t < t_pulse_end:
+        t = parser.next(t, "pulse_sample")
+        (amp, phase, _), _, _ = parser.sample(t)
+        time_shape.append(t)
+        amp_shape.append(amp)
+        phase_shape.append(phase)
+    # Now plot the pulse
+
+# Plot ADCs
+t = t_start
+while t < t_end:
+    t = parser.next(t, "adc_start")
+    if not t:
+        break
+    t_adc_end = parser.next(t, "adc_end")
+
+    _, _, (adc_phase, adc_freq) = parser.sample((t_start + t_end) / 2)
+
+    samples = []
+    while t < t_adc_end:
+        t = parser.next(t, "adc_sample")
+        samples.append(t)
+    # Now plot the ADC
+
+# Plot gradients
+t = t_start
+while t < t_end:
+    t = parser.next(t, "gradient_start")
+    if not t:
+        break
+    t_grad_end = parser.next(t, "gradient_end")
+
+    time_shape = []
+    gx_shape = []
+    gy_shape = []
+    gz_shape = []
+    while t < t_grad_end:
+        t = parser.next(t, "gradient_sample")
+        _, (gx, gy, gz), _ = parser.sample(t)
+        time_shape.append(t)
+        gx_shape.append(gx)
+        gy_shape.append(gy)
+        gz_shape.append(gz)
+    # Now plot the gradient
---
+++ pydissect.pyi
@@ -0,0 +1,41 @@
+from typing import Literal
+# TODO: These are really basic descriptions, replace by properly formatted and detailed documentation
+
+
+class SeqParser:
+    """The seq parser is a class that takes the .seq source and parses it,
+    in order to provede query functions that simulations can use."""
+
+    def __init__(self, source: str) -> None:
+        """Construct a SeqParser from the .seq source code."""
+
+    def time_range() -> tuple[float, float]:
+        """Return the time range in which sequence lies. These are upper
+        bounds, the actual sequence might be shorter."""
+
+    def next(self, t_start: float, poi: Literal[
+             'pulse_start', 'pulse_sample', 'pulse_end',
+             'gradient_start', 'gradient_sample', 'gradient_end',
+             'adc_start', 'adc_sample', 'adc_end']
+             ) -> float | None:
+        """Return the time point of the next point of interest (POI), beginning
+        with the provided starting time. Can return None if no POI of the given
+        type was found."""
+
+    def integrate(self, t0: float, t1: float
+                  ) -> tuple[tuple[float, float], tuple[float, float, float]]:
+        """Returns the integration result over the time [t0, t1].
+
+        Returns (flip_angle, phase), (gx, gy, gz)
+        """
+
+    def sample(self, t: float) -> tuple[tuple[float, float, float],
+                                        tuple[float, float, float],
+                                        tuple[float, float] | tuple[None, None]
+                                        ]:
+        """Returns the current rf and gradient amplitudes as well as adc phase
+        (if there is an adc block active) for the given time point.
+
+        Returns (pulse_amplitude, pulse_phase, pulse_frequency),
+        (gx, gy, gz), (adc_phase, adc_frequency)
+        """
