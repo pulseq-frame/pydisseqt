760769d1e7e32fa347720de0d0f962d9028e2e73
J-Endres <jonathan.endres@uk-erlangen.de>
2024-01-23 15:34:53 +0100

    Updated importer example, now seems to work with all files

--- examples/mr0_importer.py
+++ examples/mr0_importer.py
@@ -1,5 +1,4 @@
 # %%
-import cProfile
 import pydisseqt
 import MRzeroCore as mr0
 import numpy as np
@@ -9,7 +8,9 @@ import numpy as np
 
 
 # %%
-def import_pulseq(path: str) -> mr0.Sequence:
+def import_pulseq(path: str,
+                  overwrite_fov: tuple[float, float, float] | None = None
+                  ) -> mr0.Sequence:
     parser = pydisseqt.load_pulseq(path)
     seq = mr0.Sequence()
     t = 0
@@ -20,13 +21,17 @@ def import_pulseq(path: str) -> mr0.Sequence:
         else:
             return mr0.PulseUsage.REFOC
 
-    fov = parser.fov()
-    if fov is None:
-        fov = (1, 1, 1)
+    if overwrite_fov is not None:
+        fov = overwrite_fov
+    else:
+        fov = parser.fov()
+        if fov is None:
+            fov = (1, 1, 1)
 
     while parser.encounter("rf", t) is not None:
         pulse_start, pulse_end = parser.encounter("rf", t)
         rep_start = (pulse_start + pulse_end) / 2
+        t = rep_start
 
         # Calculate end of repetition
         next_pulse = parser.encounter("rf", pulse_end)
@@ -37,8 +42,6 @@ def import_pulseq(path: str) -> mr0.Sequence:
 
         # Get all ADC sample times
         adc_times = parser.events("adc", rep_start, rep_end)
-        if len(adc_times) > 0:
-            t = adc_times[-1]
 
         # Now build the mr0 repetition
 
@@ -75,24 +78,30 @@ if __name__ == "__main__":
     import matplotlib.pyplot as plt
     import torch
     from time import time
+    import torchkbnufft as tkbn
 
     start = time()
-    seq = import_pulseq("gre.seq")
-    # mr0.Sequence.from_seq_file("gre.seq")
+    # seq = import_pulseq("../../test-seqs/pypulseq/1.4.0/haste.seq")
+    seq = import_pulseq("../../test-seqs/spiral-TSE/ssTSE.seq", (0.24, 0.24, 1))
     print(f"Importing took {time() - start} seconds")
+    seq.plot_kspace_trajectory((15, 15), "xy", False)
+
     phantom = mr0.VoxelGridPhantom.brainweb("subject04.npz")
-    data = phantom.interpolate(65, 65, 32).slices([16]).build()
+    data = phantom.interpolate(128, 128, 32).slices([16]).build()
 
     graph = mr0.compute_graph(seq, data)
     signal = mr0.execute_graph(graph, seq, data)
-    reco = reco = torch.fft.fftshift(torch.fft.ifft2(
-        torch.fft.ifftshift(signal.view(256, 256))))
 
-    plt.figure()
-    plt.imshow(reco.abs(), origin='lower', vmin=0)
-    plt.colorbar()
+    # NUFFT Reconstruction
+    res = [256, 256]
+    kspace = seq.get_kspace()[:, :2] / 30
+    dcomp = tkbn.calc_density_compensation_function(kspace.T, res)
+    nufft_adj = tkbn.KbNufftAdjoint(res, [res[0]*2, res[1]*2])
+    reco = nufft_adj(signal[None, None, :, 0] * dcomp, kspace.T)[0, 0, ...]
+
+    plt.figure(figsize=(7, 7), dpi=120)
+    plt.imshow(reco.abs().T, origin='lower', vmin=0)
+    plt.axis("off")
     plt.show()
-
-    cProfile.run('import_pulseq("gre.seq")', sort="cumtime")
 
 # %%
