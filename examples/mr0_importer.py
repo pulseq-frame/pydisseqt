c98d0fd12cefa0d36c4a3c0873ef9f6c5ae654ae
J-Endres <67545816+J-Endres@users.noreply.github.com>
2024-01-24 11:30:45 +0100

    Completed importer: now supports exact gradient trajectories

--- examples/mr0_importer.py
+++ examples/mr0_importer.py
@@ -2,6 +2,7 @@
 import pydisseqt
 import MRzeroCore as mr0
 import numpy as np
+from time import time
 
 # NOTE: This importer is not diffusion-save.
 # For this, add more events at appropriate positions (maybe gated by a flag)
@@ -10,7 +11,9 @@ import numpy as np
 # %%
 def import_file(file_name: str,
                 default_fov: tuple[float, float, float] = (1, 1, 1),
-                overwrite_fov: bool = False
+                overwrite_fov: bool = False,
+                exact_trajectories: bool = True,
+                print_stats: bool = False
                 ) -> mr0.Sequence:
     """Import a pulseq .seq file.
 
@@ -22,18 +25,24 @@ def import_file(file_name: str,
         If no FOV is provided by the file, use this default value
     overwrite_fov : bool
         If true, use `default_fov` even if the file provides an FOV value
+    exact_trajectories : bool
+        If true, the gradients before and after the ADC blocks are imported
+        exactly. If false, they are summed into a single event. Depending on
+        the sequence, simulation might be faster if set to false, but the
+        simulated diffusion changes with simplified trajectoreis.
+    print_stats : bool
+        If set to true, additional information is printed during import
 
     Returns
     -------
     mr0.Sequence
         The imported file as mr0 Sequence
-
-    Note
-    ----
-    This function itself is not specific to pulseq, but supports whatever
-    pydisseqt supports. In the future, other sequence formats might be added.
     """
+    start = time()
     parser = pydisseqt.load_pulseq(file_name)
+    if print_stats:
+        print(f"Importing the .seq file took {time() - start} s")
+    start = time()
     seq = mr0.Sequence()
 
     fov = parser.fov()
@@ -65,12 +74,65 @@ def import_file(file_name: str,
 
         # Fetch additional data needed for building the mr0 sequence
         pulse = parser.integrate_one(pulses[i][0], pulses[i][1]).pulse
-        adc_times = parser.events("adc", rep_start, rep_end)
-        abs_times = [rep_start] + adc_times + [rep_end]
-        event_count = len(adc_times) + 1
 
+        adc_times = parser.events("adc", rep_start, rep_end)
+
+        # To simulate diffusion, we want to more exactly simulate gradient
+        # trajectories between pulses and the ADC block
+        if exact_trajectories:
+            # First and last timepoint in repetition with a gradient sample
+            first = pulses[i][1]
+            last = (pulses[i + 1][0] if i + 1 < len(pulses) else rep_end)
+            eps = 1e-6  # Move a bit past start / end of repetition
+            # Gradient samples can be duplicated (x, y, z with identical times)
+            # They are deduplicated after rounding to `precision` digits
+            precision = 6
+
+            if len(adc_times) > 0:
+                grad_before = sorted(set([round(t, precision) for t in (
+                    parser.events("grad x", first + eps, adc_times[0] - eps) +
+                    parser.events("grad y", first + eps, adc_times[0] - eps) +
+                    parser.events("grad z", first + eps, adc_times[0] - eps)
+                )]))
+                grad_after = sorted(set([round(t, precision) for t in (
+                    parser.events("grad x", adc_times[-1] + eps, last - eps) +
+                    parser.events("grad y", adc_times[-1] + eps, last - eps) +
+                    parser.events("grad z", adc_times[-1] + eps, last - eps)
+                )]))
+                # Last repetition: there is no pulse, ignore [last, rep_end]
+                if i == len(pulses) - 1:
+                    abs_times = [rep_start, first] + grad_before + adc_times
+                else:
+                    abs_times = ([rep_start, first] + grad_before + adc_times +
+                                 grad_after + [last, rep_end])
+                # Index of first ADC: -1 because we count spans between indices
+                adc_start = 2 + len(grad_before) - 1
+            else:
+                grad = sorted(set([round(t, precision) for t in (
+                    parser.events("grad x", first + eps, last - eps) +
+                    parser.events("grad y", first + eps, last - eps) +
+                    parser.events("grad z", first + eps, last - eps)
+                )]))
+                # Last repetition: there is no pulse, ignore [last, rep_end]
+                if i == len(pulses) - 1:
+                    abs_times = [rep_start, first] + grad
+                else:
+                    abs_times = ([rep_start, first] + grad + [last, rep_end])
+                adc_start = None
+        else:
+            # No gradient samples, only adc and one final to the next pulse
+            abs_times = [rep_start] + adc_times + [rep_end]
+            adc_start = 0
+
+        event_count = len(abs_times) - 1
         samples = parser.sample(adc_times)
         moments = parser.integrate(abs_times)
+
+        if print_stats:
+            print(
+                f"Rep. {i + 1}: {event_count} samples, of which "
+                f"{len(adc_times)} are ADC (starting at {adc_start})"
+            )
 
         # -- Now we build the mr0 Sequence repetition --
 
@@ -85,9 +147,13 @@ def import_file(file_name: str,
         rep.gradm[:, 1] = torch.as_tensor(moments.gradient.y) * fov[1]
         rep.gradm[:, 2] = torch.as_tensor(moments.gradient.z) * fov[2]
 
-        rep.adc_usage[:-1] = 1
-        rep.adc_phase[:-1] = np.pi / 2 - torch.as_tensor(samples.adc.phase)
+        if adc_start is not None:
+            phases = np.pi / 2 - torch.as_tensor(samples.adc.phase)
+            rep.adc_usage[adc_start:adc_start + len(adc_times)] = 1
+            rep.adc_phase[adc_start:adc_start + len(adc_times)] = phases
 
+    if print_stats:
+        print(f"Converting the sequence to mr0 took {time() - start} s")
     return seq
 
 
@@ -107,14 +173,11 @@ def current_fig_as_img(dpi: float = 180) -> np.ndarray:
 if __name__ == "__main__":
     import matplotlib.pyplot as plt
     import torch
-    from time import time
     import torchkbnufft as tkbn
     import imageio
 
-    start = time()
     # seq = import_pulseq("../../test-seqs/pypulseq/1.4.0/haste.seq")
     seq = import_file("../../test-seqs/spiral-TSE/ssTSE.seq", (0.24, 0.24, 1))
-    print(f"Importing took {time() - start} seconds")
     seq.plot_kspace_trajectory((7, 7), "xy", False)
 
     phantom = mr0.VoxelGridPhantom.brainweb("subject04.npz")
@@ -122,9 +185,9 @@ if __name__ == "__main__":
     B0 = data.B0.clone()
 
     gif = []
-    img_count = 3
-    for i, dB0 in enumerate(np.linspace(-5, 5, img_count)):
-        print(f"{i + 1} / {img_count}")
+    B0_factors = [1]  # np.linspace(-5, 5, 150)
+    for i, dB0 in enumerate(B0_factors):
+        print(f"{i + 1} / {len(B0_factors)}")
 
         data.B0 = dB0 * B0
         graph = mr0.compute_graph(seq, data)
@@ -152,8 +215,13 @@ if __name__ == "__main__":
                    vmin=-np.pi, vmax=np.pi, cmap="twilight")
         plt.axis("off")
         plt.subplots_adjust(wspace=0.02)
-        gif.append(current_fig_as_img(80))
-        plt.close()
-    imageio.mimsave("B0 Spiral.gif", gif, fps=1, loop=0)
+        if len(B0_factors) == 1:
+            plt.show()
+        else:
+            gif.append(current_fig_as_img(80))
+            plt.close()
+
+    if len(gif) > 0:
+        imageio.mimsave("B0 Spiral.gif", gif, fps=1, loop=0)
 
 # %%
